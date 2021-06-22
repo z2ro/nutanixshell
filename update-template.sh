a01c1cfc3ab800bb8f07e2cc1b90f95d119b59b5
Aleksander Pinheiro <67646712+z2ro@users.noreply.github.com>
2021-06-22 12:08:44 -0300

    Create update-template.sh

---
+++ update-template.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+read -p "IP: " VM_IP
+echo
+echo "CENTOS7, UBUNTU18, UBUNTU20"
+read -p "Choose your destiny: " CYD
+echo
+
+UUID=$(bash get-uuid.sh $CYD)
+
+if [ $CYD == "CENTOS7" ]; then
+    echo "Acessando a VM e atualizando"
+    echo password | ssh -tt user@$VM_IP 'sudo yum update -y' >> ./image_centos7_update.log
+    echo
+    echo "Apagando a imagem atual $CYD ..."
+    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    echo
+    sleep 5
+    echo "Aguarde..."
+    echo
+    echo "Criando imagem nova $CYD ..."
+    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+fi
+
+if [ $CYD == "UBUNTU18" ]; then
+    echo "Acessando a VM e atualizando"
+    echo password | ssh -tt user@$VM_IP 'sudo apt upgrade && sudo apt update -y' >> ./image_ubuntu18_update.log
+    echo
+    echo "Apagando a imagem atual $CYD ..."
+    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    echo
+    sleep 5
+    echo "Aguarde..."
+    echo
+    echo "Criando imagem nova $CYD ..."
+    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+fi
+
+if [ $CYD == "UBUNTU20" ]; then
+    echo "Acessando a VM e atualizando"
+    echo password | ssh -tt user@$VM_IP 'sudo apt upgrade && sudo apt update -y' >> ./image_ubuntu20_update.log
+    echo
+    echo "Apagando a imagem atual $CYD ..."
+    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    echo
+    sleep 5
+    echo "Aguarde..."
+    echo
+    echo "Criando imagem nova $CYD ..."
+    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+fi
