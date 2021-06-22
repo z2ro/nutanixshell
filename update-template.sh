e99c2461a2aa337bf05467009c3d5d8c80946f9e
Aleksander Pinheiro <67646712+z2ro@users.noreply.github.com>
2021-06-22 12:43:48 -0300

    Update update-template.sh

--- update-template.sh
+++ update-template.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+read -p "Nutanix IP: " NTX_IP
 read -p "IP: " VM_IP
 echo
 echo "CENTOS7, UBUNTU18, UBUNTU20"
@@ -13,13 +14,13 @@ if [ $CYD == "CENTOS7" ]; then
     echo password | ssh -tt user@$VM_IP 'sudo yum update -y' >> ./image_centos7_update.log
     echo
     echo "Apagando a imagem atual $CYD ..."
-    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    curl --insecure --request DELETE --url https://$NTX_IP:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
     echo
     sleep 5
     echo "Aguarde..."
     echo
     echo "Criando imagem nova $CYD ..."
-    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+    curl --insecure --request POST --url https://$NTX_IP:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
 fi
 
 if [ $CYD == "UBUNTU18" ]; then
@@ -27,13 +28,13 @@ if [ $CYD == "UBUNTU18" ]; then
     echo password | ssh -tt user@$VM_IP 'sudo apt upgrade && sudo apt update -y' >> ./image_ubuntu18_update.log
     echo
     echo "Apagando a imagem atual $CYD ..."
-    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    curl --insecure --request DELETE --url https://$NTX_IP:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
     echo
     sleep 5
     echo "Aguarde..."
     echo
     echo "Criando imagem nova $CYD ..."
-    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+    curl --insecure --request POST --url https://$NTX_IP:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
 fi
 
 if [ $CYD == "UBUNTU20" ]; then
@@ -41,11 +42,11 @@ if [ $CYD == "UBUNTU20" ]; then
     echo password | ssh -tt user@$VM_IP 'sudo apt upgrade && sudo apt update -y' >> ./image_ubuntu20_update.log
     echo
     echo "Apagando a imagem atual $CYD ..."
-    curl --insecure --request DELETE --url https://x.x.x.x:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
+    curl --insecure --request DELETE --url https://$NTX_IP:9440/api/nutanix/v2.0/images/$UUID --header 'authorization: Basic auth'
     echo
     sleep 5
     echo "Aguarde..."
     echo
     echo "Criando imagem nova $CYD ..."
-    curl --insecure --request POST --url https://x.x.x.x:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
+    curl --insecure --request POST --url https://$NTX_IP:9440/api/nutanix/v2.0/images/ --header 'authorization: Basic auth' --header 'content-type: application/json' --data '{"image_import_spec": {"container_name": "string","url": "nfs://127.0.0.1/CONTAINER/.acropolis/vmdisk/uuid"},"image_type": "DISK_IMAGE","name": "TEMPLATE-NAME"}'
 fi
