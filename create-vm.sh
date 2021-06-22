3905e008b49868f6670d171d1d817c6977cf7dd8
Aleksander Pinheiro <67646712+z2ro@users.noreply.github.com>
2021-06-22 12:00:49 -0300

    Create create-vm.sh

---
+++ create-vm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "VM NAME: " V_NAME
+read -p "VCPU: " V_VCPU
+read -p "MEM: " V_MEM
+
+URL=https://x.x.x.x:9440/api/nutanix/v3/vms
+
+DATAC=$(cat DATA-VM.json | jq --compact-output '.')
+
+DATAC1=$(echo $DATAC | jq --compact-output --arg a "$V_NAME" '.spec.name = $a')
+
+DATAC2=$(echo $DATAC1 | jq --compact-output --arg a "$V_VCPU" '.spec.resources.num_sockets = $a')
+
+DATAC3=$(echo $DATAC2 | jq --compact-output --arg a "$V_MEM" '.spec.resources.memory_size_mib = $a')
+
+DATAC4=$(echo $DATAC3 | jq --compact-output --arg a "$V_MEM" '.spec.resources.memory_size_mib = $a')
+
+echo -e "curl -v --insecure --request POST --url $URL --header 'content-type: application/json' --header 'authorization: Basic auth' --data "$DATAC2""
